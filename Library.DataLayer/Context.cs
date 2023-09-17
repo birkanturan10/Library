8858201abe6160e80e42d4c49f429cfbfc9805f6
birkanturan10 <117521602+birkanturan10@users.noreply.github.com>
2023-09-17 12:10:39 +0300

    Library

---
+++ Library.DataLayer/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.DataLayer
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-OERL0I6O; Database=Library; Trusted_Connection=true");
+        }
+    }
+}
