adc35ce2f917a5d9da857456a1fec043380be96e
birkanturan10 <117521602+birkanturan10@users.noreply.github.com>
2023-09-18 10:26:37 +0300

    Migration oluşturuldu. MSSQL Server ile bağlantı kuruldu.

---
+++ Library.DataLayer/Migrations/20230918070848_LibraryCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace Library.DataLayer.Migrations
+{
+    public partial class LibraryCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+        name: "BookLists",
+        columns: table => new
+        {
+            BookID = table.Column<int>(nullable: false)
+                .Annotation("SqlServer:Identity", "1, 1"),
+            BookName = table.Column<string>(nullable: true),
+            Author = table.Column<string>(nullable: true),
+            NumberOfPages = table.Column<int>(nullable: false),
+            PublicationDate = table.Column<DateTime>(nullable: false),
+            Publisher = table.Column<string>(nullable: true)
+        },
+        constraints: table =>
+        {
+            table.PrimaryKey("PK_BookLists", x => x.BookID);
+        });
+    }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+       name: "BookLists");
+        }
+    }
+}
---
+++ Library.TypeLayer/BookList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.TypeLayer
+{
+    public class BookList
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DisplayName("ID")]
+        public int BookID { get; set; }
+
+        [DisplayName("Kitap Adı")]
+        public string BookName { get; set; }
+
+        [DisplayName("Yazar")]
+        public string Author { get; set; }
+
+        [DisplayName("Sayfa Sayısı")]
+        public int NumberOfPages { get; set; }
+
+        [DisplayName("Basım Tarihi")]
+        public DateTime PublicationDate { get; set; }
+
+        [DisplayName("Yayınevi")]
+        public string Publisher { get; set; }
+    }
+}
