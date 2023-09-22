f8b97b05824162442385a3682f869db93e5d6d3a
birkanturan10 <117521602+birkanturan10@users.noreply.github.com>
2023-09-22 12:31:32 +0300

    Kitap ekleme metodu yapıldı. Artık proje üzerinden kitap ekleme işlemi yapılabiliyor ve SQL'e düşüyor.

--- Library.UI/Controllers/HomeController.cs
+++ Library.UI/Controllers/HomeController.cs
@@ -1,15 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Library.DataLayer;
+using Library.TypeLayer;
 
 namespace Library.UI.Controllers
 {
-    public class HomeController : Controller
-    {
-        Context context = new Context();
+	public class HomeController : Controller
+	{
+		Context context = new Context();
 
-        public IActionResult Index()
-        {
-            return View(context.BookLists);
-        }
-    }
+		public IActionResult Index()
+		{
+			return View(context.BookLists);
+		}
+
+		public IActionResult AddBook()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult AddBook([Bind("BookName,Author,NumberOfPages,PublicationDate,Publisher")] BookList bookList)
+		{
+			if (ModelState.IsValid)
+			{
+				context.Add(bookList);
+				context.SaveChanges();
+			}
+			return View(bookList);
+		}
+	}
 }
