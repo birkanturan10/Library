ab08fc65170b85aef9c89b8a8855450cbf3a58e9
birkanturan10 <117521602+birkanturan10@users.noreply.github.com>
2023-09-23 17:40:47 +0300

    Güncelleme ve silme metodları da eklenerek CRUD işlemleri tamamlandı.

--- Library.UI/Controllers/HomeController.cs
+++ Library.UI/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Library.DataLayer;
 using Library.TypeLayer;
+using Microsoft.EntityFrameworkCore;
 
 namespace Library.UI.Controllers
 {
@@ -25,8 +26,99 @@ namespace Library.UI.Controllers
 			{
 				context.Add(bookList);
 				context.SaveChanges();
+				return RedirectToAction(nameof(Index));
 			}
 			return View(bookList);
+		}
+
+		public IActionResult Update(int? ID)
+		{
+			if (ID==null||context.BookLists==null)
+			{
+				return NotFound();
+			}
+
+			var bookList = context.BookLists.Find(ID);
+
+			if (bookList==null) 
+			{
+				return NotFound();
+			}
+
+			return View(bookList);
+		}
+
+		[HttpPost]
+		public IActionResult Update(int ID, [Bind("BookID,BookName,Author,NumberOfPages,PublicationDate,Publisher")]BookList bookList)
+		{
+			if (ID!=bookList.BookID)
+			{
+				return NotFound();
+			}
+
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					context.Update(bookList);
+					context.SaveChanges();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!BookListExists(bookList.BookID))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(bookList);
+		}
+
+		private bool BookListExists(int ID)
+		{
+			return context.BookLists.Any(x => x.BookID == ID);
+		}
+
+		public IActionResult Delete(int? ID)
+		{
+			if (ID == null || context.BookLists == null)
+			{
+				return NotFound();
+			}
+
+			var bookList = context.BookLists.FirstOrDefault(x => x.BookID == ID);
+
+			if (bookList == null)
+			{
+				return NotFound();
+			}
+
+			return View(bookList);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeleteConfirmed(int ID)
+		{
+			if (context.BookLists == null)
+			{
+				return Problem("Entity set 'context.Students'  is null.");
+			}
+
+			var bookList = context.BookLists.Find(ID);
+
+			if (bookList != null)
+			{
+				context.BookLists.Remove(bookList);
+			}
+
+			context.SaveChanges();
+
+			return RedirectToAction(nameof(Index));
 		}
 	}
 }
